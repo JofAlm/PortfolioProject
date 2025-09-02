566894835c5835c80bbfad397661797774549d21
Jens Alm <jensalm@Jenss-MacBook-Pro.local>
2025-09-02 13:47:17 +0200

    fix: Corrrected invalid viewBox attribute in close button SVG

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -125,7 +125,7 @@ const StartPage = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
                 fill="none"
-                viewBox="0 0 24"
+                viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
